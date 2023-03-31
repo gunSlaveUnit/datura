0c51562018c8b8105642a9c4751aad7e6905ea21
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2023-03-31 21:23:53 +0300

    Role entity

---
+++ server/src/models/role.py
@@ -0,0 +1,10 @@
+from sqlalchemy import Column, Enum
+
+from server.src.models.entity import Entity
+from server.src.settings import RoleType
+
+
+class Role(Entity):
+    __tablename__ = "roles"
+
+    title = Column(Enum(RoleType), unique=True, index=True, nullable=False)
