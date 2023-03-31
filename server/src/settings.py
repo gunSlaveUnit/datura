a8e8ef1f8060f7dfdfbcfa3a992622b0f4213d4d
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2023-03-31 20:07:04 +0300

    Database and redis configuration

--- server/src/settings.py
+++ server/src/settings.py
@@ -1,5 +1,30 @@
-# Versions
 from enum import Enum
+from pathlib import Path
+
+from dotenv import dotenv_values
+
+DEBUG = False
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+ENVS_DIR = BASE_DIR / 'envs'
+
+if DEBUG:
+    ENVS_DIR = ENVS_DIR / 'dev'
+else:
+    ENVS_DIR = ENVS_DIR / 'prod'
+
+# Databases
+
+redis_config = dotenv_values(ENVS_DIR / ".redis")
+database_config = dotenv_values(ENVS_DIR / ".db")
+
+if DEBUG:
+    CONNECTION_STRING = f"sqlite:///{BASE_DIR / database_config['NAME']}"
+else:
+    CONNECTION_STRING = f'postgresql://{database_config["USER"]}:{database_config["PASSWORD"]}' \
+                        f'@{database_config["HOST"]}:{database_config["PORT"]}/{database_config["NAME"]}'
+
+# Versions
 
 API_VERSION = '0.1.0'
 
