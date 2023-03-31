cc8b2620318a4862c2755728274a526b8b5ad5b1
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2023-03-31 14:44:52 +0300

    Assets router tags

--- server/src/settings.py
+++ server/src/settings.py
@@ -14,9 +14,11 @@ ASSETS_ROUTER_PREFIX = '/{game_id}/assets'
 class Tags(str, Enum):
     GAMES = 'Games'
     COMPANIES = 'Companies'
+    ASSETS = 'Assets'
 
 
 tags_metadata = [
     {'name': Tags.GAMES, 'description': 'Describes an API to manage games'},
     {'name': Tags.COMPANIES, 'description': 'Describes an  API to manage companies'},
+    {'name': Tags.ASSETS, 'description': 'Describes an API for managing game resources'},
 ]
