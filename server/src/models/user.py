3c0bde25e7e32a7e90231dcd5b0185013ac45a65
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2023-03-31 21:29:21 +0300

    User table

---
+++ server/src/models/user.py
@@ -0,0 +1,22 @@
+import datetime
+
+from sqlalchemy import Column, Integer, String, Boolean, ForeignKey
+from sqlalchemy.orm import relationship
+
+from server.src.models.entity import Entity
+
+
+class User(Entity):
+    __tablename__ = "users"
+
+    email = Column(String(255), index=True, nullable=False)
+    account_name = Column(String(255), index=True, nullable=False)
+    displayed_name = Column(String(255), index=True, nullable=False)
+    password = Column(String, nullable=False)
+    is_active = Column(Boolean, nullable=False, default=True)
+    last_login = Column(Integer, onupdate=datetime.datetime.now().timestamp())
+    is_staff = Column(Boolean, nullable=False, default=False)
+    is_superuser = Column(Boolean, nullable=False, default=False)
+    role_id = Column(Integer, ForeignKey('roles.id'), index=True, nullable=False)
+
+    role = relationship("Role", backref="users")
