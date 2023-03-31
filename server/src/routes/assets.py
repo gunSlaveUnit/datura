30b692dd47a2c7bb5fc8b7f00fd0fe2d9509f4f5
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2023-03-31 14:45:50 +0300

    Route to download a capsule

--- server/src/routes/assets.py
+++ server/src/routes/assets.py
@@ -11,3 +11,11 @@ async def download_header():
     Returns an image for the header section of the game
     """
     pass
+
+
+@router.get('/capsule/')
+async def download_capsule():
+    """
+    Returns an image for the capsule section of the game
+    """
+    pass
