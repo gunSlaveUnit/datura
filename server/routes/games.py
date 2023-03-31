52c4204422e13d0148f4a7b8477ab6736cc0df2c
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2023-03-31 11:54:56 +0300

    Test list game route

--- server/routes/games.py
+++ server/routes/games.py
@@ -1,10 +1,23 @@
-from fastapi import APIRouter
+from typing import List
 
+from fastapi import APIRouter
+from starlette import status
+from starlette.responses import Response
+
+from schemes.games import GameCreateScheme, GameDBScheme
 from settings import GAMES_ROUTER_PREFIX, Tags
 
 router = APIRouter(prefix=GAMES_ROUTER_PREFIX, tags=[Tags.GAMES])
 
 
-@router.get('/')
-def list_all():
-    return {"message": "test"}
+@router.get('/', response_model=List[GameDBScheme])
+async def every() -> List[GameDBScheme]:
+    """
+    List of all games according to the given filters.
+    Returns a list of GameDBScheme with game data.
+    """
+
+    return [
+        GameDBScheme(title="Test game title 1"),
+        GameDBScheme(title="Test game title 2"),
+    ]
