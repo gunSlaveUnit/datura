382d4475afa2842039336269cb52b57badfd961f
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2023-04-05 11:07:59 +0300

    Registered auth logic

--- desktop/src/main.py
+++ desktop/src/main.py
@@ -1,16 +1,19 @@
 import sys
 
-from PySide6.QtGui import QGuiApplication, QColor, QIcon
+from PySide6.QtGui import QGuiApplication, QColor
 from PySide6.QtQml import QQmlApplicationEngine
 
 from desktop.src.settings import LAYOUTS_DIR
-
+from desktop.src.logic.AuthLogic import AuthLogic
 
 if __name__ == '__main__':
     app = QGuiApplication(sys.argv)
     app.setPalette(QColor("black"))
 
     engine = QQmlApplicationEngine()
+
+    auth_logic = AuthLogic()
+    engine.rootContext().setContextProperty("auth_logic", auth_logic)
 
     start_file_location = LAYOUTS_DIR / "main.qml"
     engine.load(start_file_location)
