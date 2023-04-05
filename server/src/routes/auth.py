00f4e6f0b99206f2ad037429f39dea0237c9a43e
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2023-04-05 09:41:57 +0300

    Json response in dict {"detail": "some message"}

--- server/src/routes/auth.py
+++ server/src/routes/auth.py
@@ -35,14 +35,14 @@ async def sign_in(login_data: SignInSchema,
     if user is None:
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Incorrect account name or password"
+            detail={"detail": "Incorrect account name or password"}
         )
 
     session_id = str(uuid.uuid4())
     session_storage.set(session_id, user.id)
     session_storage.expire(session_id, SESSION_TTL)
 
-    response = JSONResponse({"session": session_id})
+    response = JSONResponse({"detail": "Logged in successfully"})
     response.set_cookie("session_id", session_id, max_age=SESSION_TTL)
     return response
 
