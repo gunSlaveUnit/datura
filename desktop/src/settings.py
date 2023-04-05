7a1f6cb37d7ea3ff6d5ed2c1489f868a4db762f7
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2023-04-05 11:28:48 +0300

    URLS in settings

--- desktop/src/settings.py
+++ desktop/src/settings.py
@@ -1,5 +1,17 @@
 from pathlib import Path
 
+# Paths
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 SOURCES_DIR = BASE_DIR / 'src'
 LAYOUTS_DIR = SOURCES_DIR / 'gui'
+
+# URLs
+SERVER_URL = "http://127.0.0.1:8000/"
+
+# Auth
+AUTH_URL = SERVER_URL + "auth/"
+REGISTER_URL = AUTH_URL + "sign-up/"
+LOGIN_URL = AUTH_URL + "sign-in/"
+LOGOUT_URL = AUTH_URL + "sign-out/"
+ME_URL = AUTH_URL + "me/"
