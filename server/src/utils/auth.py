7f90558f7dbeb949e3ceada19d019d1003664ad8
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2023-04-05 09:03:39 +0300

    authenticate_user function

--- server/src/utils/auth.py
+++ server/src/utils/auth.py
@@ -1,5 +1,13 @@
+from models.user import User
 from utils.crypt import crypt_context
 
 
 async def verify_password(plain_password, hashed_password):
     return crypt_context.verify(plain_password, hashed_password)
+
+
+async def authenticate_user(account_name: str, password: str, db):
+    user = db.query(User).filter(User.account_name == account_name).first()
+    if user and await verify_password(password, user.password):
+        return user
+    return None
