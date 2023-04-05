17981d2a35d744cd286c79240efa88a978f4a21b
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2023-04-05 10:28:22 +0300

    Sign up route

--- server/src/routes/auth.py
+++ server/src/routes/auth.py
@@ -5,22 +5,51 @@ from sqlalchemy.orm import Session
 from starlette import status
 from starlette.responses import JSONResponse
 
+from server.src.models.role import Role
+from server.src.schemas.auth import SignInSchema, SignUpSchema
 from server.src.models.user import User
-from server.src.schemas.user import SignInSchema, UserDBSchema
-from server.src.settings import Tags, SESSION_TTL
+from server.src.schemas.user import UserDBSchema
+from server.src.settings import Tags, SESSION_TTL, RoleType
 from server.src.utils.db import get_db, get_session_storage
 from server.src.utils.auth import authenticate_user, get_current_user
+from server.src.utils.crypt import get_password_hash
 
 router = APIRouter(prefix='/auth', tags=[Tags.AUTH])
 
 
 @router.post("/sign-up/")
-async def sign_up():
+async def sign_up(registration_data: SignUpSchema,
+                  db: Session = Depends(get_db),
+                  session_storage=Depends(get_session_storage)):
     """
     Registration (creation of a new user).
     Login immediately.
     """
-    pass
+    potentially_existing_user = db.query(User).filter(User.email == registration_data.email).first()
+
+    if potentially_existing_user:
+        raise HTTPException(
+            status_code=status.HTTP_409_CONFLICT,
+            detail={"detail": "User with the same email address or account name already exists"}
+        )
+
+    user = User(
+        email=registration_data.email,
+        account_name=registration_data.account_name,
+        displayed_name=f'Player #{db.query(User).count() + 1}',
+        password=get_password_hash(registration_data.password),
+        role_id=db.query(Role).filter(Role.title == RoleType.USER).one().id
+    )
+
+    db.add(user)
+    db.commit()
+    db.refresh(user)
+
+    return await sign_in(
+        SignInSchema(account_name=user.account_name, password=registration_data.password),
+        db,
+        session_storage
+    )
 
 
 @router.post("/sign-in/")
