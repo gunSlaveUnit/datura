efda9270863ba9d8f59b27db337dfb8cd9d8f4d7
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2023-04-05 10:06:15 +0300

    Added UserDBSchema

--- server/src/schemas/user.py
+++ server/src/schemas/user.py
@@ -1,6 +1,25 @@
+import datetime
+
 from pydantic import BaseModel
+
+from server.src.schemas.entity import EntityDBSchema
 
 
 class SignInSchema(BaseModel):
     account_name: str
     password: str
+
+
+class UserDBSchema(EntityDBSchema):
+    email: str
+    account_name: str
+    displayed_name: str
+    password: str
+    is_active: bool = True
+    last_login_at: datetime.datetime | None
+    is_staff: bool = False
+    is_superuser: bool = False
+    role_id: int
+
+    class Config:
+        orm_mode = True
