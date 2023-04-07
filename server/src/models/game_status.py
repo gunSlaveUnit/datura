f41bdf531a899da7c391839a397f5d7cce4535c3
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2023-04-07 19:38:25 +0300

    title is Enum now

--- server/src/models/game_status.py
+++ server/src/models/game_status.py
@@ -1,11 +1,12 @@
-from sqlalchemy import Column, String
+from sqlalchemy import Column, String, Enum
 from sqlalchemy.orm import relationship
 
 from server.src.models.entity import Entity
+from server.src.settings import GameStatusType
 
 
 class GameStatus(Entity):
     __tablename__ = "game_statuses"
 
-    title = Column(String, index=True, nullable=False)
+    title = Column(Enum(GameStatusType), unique=True, index=True, nullable=False)
     games = relationship("Game", back_populates="status")
