5345f5861bf2d60b5d87fdb2d2978b0149eb7765
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2023-04-07 19:49:08 +0300

    title is Enum in age model

--- server/src/models/age.py
+++ server/src/models/age.py
@@ -1,13 +1,14 @@
-from sqlalchemy import Column, String, Text
+from sqlalchemy import Column, String, Text, Enum
 from sqlalchemy.orm import relationship
 
 from server.src.models.entity import Entity
+from server.src.settings import AgeType
 
 
 class Age(Entity):
     __tablename__ = "ages"
 
-    title = Column(String, unique=True, nullable=False)
-    description =  Column(Text, nullable=False)
+    title = Column(Enum(AgeType), unique=True, index=True, nullable=False)
+    description = Column(Text, nullable=False)
 
     games = relationship("Game", back_populates="age")
