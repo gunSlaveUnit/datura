62d146a6a9024cf38d6adda768ab9c60fd6a2941
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2023-04-07 19:49:26 +0300

    Ages init

--- server/src/main.py
+++ server/src/main.py
@@ -4,13 +4,14 @@ import gzip
 from fastapi import FastAPI
 from fastapi.responses import StreamingResponse
 
+from server.src.models.age import Age
 from server.src.models.game_status import GameStatus
 from server.src.models.role import Role
 from server.src.models.user import User
 from server.src.routes.auth import router as auth_router
 from server.src.routes.games import router as games_router
 from server.src.routes.companies import router as companies_router
-from server.src.settings import tags_metadata, RoleType, admin_config, GameStatusType
+from server.src.settings import tags_metadata, RoleType, admin_config, GameStatusType, AgeType
 from server.src.utils.crypt import get_password_hash
 from server.src.utils.db import Base, engine, get_db
 
@@ -41,6 +42,9 @@ def init_db():
     if len(db.query(GameStatus).all()) == 0:
         _add_game_statuses(db)
 
+    if len(db.query(Age).all()) == 0:
+        _add_ages(db)
+
 
 def _add_admin(session):
     admin_role = session.query(Role).filter(Role.title == RoleType.ADMIN).one()
@@ -70,6 +74,31 @@ def _add_game_statuses(session):
     for game_status_type in GameStatusType:
         game_status = GameStatus(title=game_status_type)
         session.add(game_status)
+    session.commit()
+
+
+def _add_ages(session):
+    pegi_3 = Age(title=AgeType.PEGI_3, description="Suitable for all age categories. The game must not contain sounds "
+                                                   "or images that may frighten young children. Profanity must not be"
+                                                   " used.")
+    pegi_7 = Age(title=AgeType.PEGI_7, description="Suitable for people over 7 years old. The game may contain "
+                                                   "moderate violence and some scenes may scare children.")
+    pegi_12 = Age(title=AgeType.PEGI_12, description="Suitable for people over 12 years old. The game may contain "
+                                                     "more realistic and naturalistic scenes of violence.")
+    pegi_16 = Age(title=AgeType.PEGI_16, description="Suitable for people over 16 years old. This rating is used when "
+                                                     "the violence becomes realistic and lifelike. Profanity, "
+                                                     "use of tobacco, alcohol, or illegal drugs may also be present.")
+    pegi_18 = Age(title=AgeType.PEGI_18, description='Suitable for persons over 18 years of age. The "adult" '
+                                                     'classification is used when an extreme level of violence and '
+                                                     'unmotivated murder is present in the game. Romanticization of '
+                                                     'drugs, gambling, and sexual activity may also be present.')
+
+    session.add(pegi_3)
+    session.add(pegi_7)
+    session.add(pegi_12)
+    session.add(pegi_16)
+    session.add(pegi_18)
+
     session.commit()
 
 
