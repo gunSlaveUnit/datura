734cae553259eaf5ad860507f60eaebbeaeff9e7
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2023-04-07 20:16:40 +0300

    GameCreateSchema and GameDBSchema are right

--- server/src/schemas/game.py
+++ server/src/schemas/game.py
@@ -9,8 +9,19 @@ class GameApprovingSchema(BaseModel):
 
 class GameCreateSchema(BaseModel):
     title: str
+    release_date: int | None
+    developer: str
+    publisher: str
+    short_description: str
+    long_description: str
+    price: float
+    status_id: int
+    age_id: int
 
 
 class GameDBSchema(GameCreateSchema, EntityDBSchema):
+    directory: str
+    author_id: int
+
     class Config:
         orm_mode = True
