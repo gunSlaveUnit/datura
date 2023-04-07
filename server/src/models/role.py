dbd0207993088ab36cd760c1ac0656495aee58c5
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2023-04-07 12:49:15 +0300

    Relationship with games

--- server/src/models/role.py
+++ server/src/models/role.py
@@ -1,10 +1,15 @@
 from sqlalchemy import Column, Enum
+from sqlalchemy.orm import relationship
 
 from server.src.models.entity import Entity
 from server.src.settings import RoleType
 
 
 class Role(Entity):
+    # TODO: Should we be able to add / edit roles? Do we need Enum here?
+
     __tablename__ = "roles"
 
     title = Column(Enum(RoleType), unique=True, index=True, nullable=False)
+
+    users = relationship("User", back_populates="role")
