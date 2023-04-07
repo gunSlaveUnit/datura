15d01158cd54b18954f7c9fd5c3245ec1268bfa9
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2023-04-07 13:33:56 +0300

    Tag models

---
+++ server/src/models/tag.py
@@ -0,0 +1,21 @@
+from sqlalchemy import Column, String, Text, Integer, ForeignKey
+from sqlalchemy.orm import relationship
+
+from server.src.models.entity import Entity
+
+
+class GameTagAssociation(Entity):
+    __tablename__ = 'games_tags'
+
+    game_id = Column(Integer, ForeignKey('games.id'), primary_key=True)
+    tag_id = Column(Integer, ForeignKey('tags.id'), primary_key=True)
+
+    game = relationship("Game", backref="tags")
+    tag = relationship("Tag", backref="games")
+
+
+class Tag(Entity):
+    __tablename__ = "tags"
+
+    title = Column(String, unique=True, nullable=False)
+    description =  Column(Text, nullable=False)
