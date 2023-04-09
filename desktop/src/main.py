58681db2033d5bc820902c83d9af62d30f92094b
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2023-04-09 23:29:32 +0300

    Registered game model

--- desktop/src/main.py
+++ desktop/src/main.py
@@ -3,6 +3,7 @@ import sys
 from PySide6.QtGui import QGuiApplication, QColor
 from PySide6.QtQml import QQmlApplicationEngine
 
+from desktop.src.models.game import GameList
 from desktop.src.settings import LAYOUTS_DIR
 from desktop.src.logic.AuthLogic import AuthLogic
 from services.AuthService import AuthService
@@ -17,6 +18,9 @@ if __name__ == '__main__':
 
     auth_logic = AuthLogic(auth_service)
     engine.rootContext().setContextProperty("auth_logic", auth_logic)
+
+    game_list_model = GameList(auth_service)
+    engine.rootContext().setContextProperty("game_list_model", game_list_model)
 
     start_file_location = LAYOUTS_DIR / "main.qml"
     engine.load(start_file_location)
