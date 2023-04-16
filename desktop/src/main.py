cda04e065b099e2fc594577e973e3cf490e23d1d
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2023-04-16 12:27:59 +0300

    Store detailed logic bound

--- desktop/src/main.py
+++ desktop/src/main.py
@@ -4,6 +4,7 @@ from PySide6.QtGui import QGuiApplication, QColor
 from PySide6.QtQml import QQmlApplicationEngine
 
 from desktop.src.logic.LibraryDetaledLogic import LibraryDetailedLogic
+from desktop.src.logic.StoreDetaledLogic import StoreDetailedLogic
 from desktop.src.models.game import GameList
 from desktop.src.settings import LAYOUTS_DIR
 from desktop.src.logic.AuthLogic import AuthLogic
@@ -22,6 +23,9 @@ if __name__ == '__main__':
 
     library_detailed_logic = LibraryDetailedLogic(auth_service)
     engine.rootContext().setContextProperty("library_detailed_logic", library_detailed_logic)
+
+    store_detailed_logic = StoreDetailedLogic(auth_service)
+    engine.rootContext().setContextProperty("store_detailed_logic", store_detailed_logic)
 
     game_list_model = GameList(auth_service)
     engine.rootContext().setContextProperty("game_list_model", game_list_model)
