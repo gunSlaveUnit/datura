f297cf3be670f9fbaa9e748106b00011d5542825
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2023-04-17 10:13:30 +0300

    Screenshots info and downloading by stream

--- server/src/routes/assets.py
+++ server/src/routes/assets.py
@@ -4,12 +4,14 @@ from typing import List
 
 from fastapi import APIRouter, UploadFile, Depends
 from sqlalchemy.orm import Session
-from starlette.responses import FileResponse
+from starlette.responses import FileResponse, StreamingResponse
 
 from server.src.models.game import Game
-from server.src.settings import ASSETS_ROUTER_PREFIX, GAMES_ASSETS_PATH, GAMES_ASSETS_CAPSULE_DIR
+from server.src.settings import ASSETS_ROUTER_PREFIX, GAMES_ASSETS_PATH, GAMES_ASSETS_CAPSULE_DIR, \
+    GAMES_ASSETS_SCREENSHOTS_DIR
 from server.src.routes.builds import router as builds_router
 from server.src.utils.db import get_db
+from server.src.utils.io import read_chunks, CHUNK_SIZE, store
 
 router = APIRouter(prefix=ASSETS_ROUTER_PREFIX)
 router.include_router(builds_router)
@@ -99,12 +101,27 @@ async def delete_capsule():
 
 
 @router.get('/screenshots/')
-async def screenshots_info(filename: str | None = None):
+async def screenshots_info(game_id: int,
+                           db: Session = Depends(get_db),
+                           filename: str | None = None):
     """
     Returns the names of the screenshot files.
     If "filename" query param was provided, returns a file.
     """
-    pass
+    game = db.query(Game).filter(Game.id == game_id).one()
+
+    path = Path(GAMES_ASSETS_PATH).joinpath(game.directory, GAMES_ASSETS_SCREENSHOTS_DIR)
+
+    if filename:
+        headers = {"Content-Disposition": f"filename={filename}"}
+
+        return StreamingResponse(
+            read_chunks(path.joinpath(filename), CHUNK_SIZE),
+            headers=headers,
+            media_type="image/webp"
+        )
+    else:
+        return {"filenames": [f.name for f in path.iterdir() if f.is_file()]}
 
 
 @router.post('/screenshots/')
