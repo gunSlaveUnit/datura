8bf409879c6b4a23f0aaa34076fbadfff2ce4cbb
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2023-04-18 18:22:01 +0300

    Added a method to load personal releases

--- desktop/src/models/game.py
+++ desktop/src/models/game.py
@@ -6,6 +6,7 @@ from PySide6.QtCore import QAbstractListModel, QModelIndex, QByteArray, Qt, Slot
 
 from desktop.src.models.entity import Entity
 from desktop.src.services.AuthService import AuthService
+from desktop.src.services.CompanyService import CompanyService
 from desktop.src.settings import LIBRARY_URL, GAMES_URL
 
 
@@ -25,10 +26,12 @@ class Game(Entity):
 
 
 class GameList(QAbstractListModel):
-    def __init__(self, auth_service: AuthService):
+    def __init__(self, auth_service: AuthService,
+                 company_service: CompanyService):
         super().__init__()
 
         self._auth_service = auth_service
+        self._company_service = company_service
 
         self._games = []
 
@@ -37,7 +40,8 @@ class GameList(QAbstractListModel):
         current_user_id = self._auth_service.current_user["id"]
         headers = {"Authorization": self._auth_service.session_id}
 
-        library_records = requests.get(LIBRARY_URL + f"?user_id={current_user_id}&include_games=true", headers=headers).json()
+        library_records = requests.get(LIBRARY_URL + f"?user_id={current_user_id}&include_games=true",
+                                       headers=headers).json()
         self.beginResetModel()
         self._games = []
         for record in library_records:
@@ -55,6 +59,16 @@ class GameList(QAbstractListModel):
         for game in games:
             self._games.append(Game(**game))
         self.endResetModel()
+
+    @Slot()
+    def load_personal(self):
+        self._company_service.load_personal()
+        if self._company_service.company is not None:
+            url = GAMES_URL + f'?company_id={self._company_service.company["id"]}'
+            data = requests.get(url, headers={"Authorization": self._auth_service.session_id}).json()
+            self.beginResetModel()
+            self._games = [Game(**detailed_game_data) for detailed_game_data in data]
+            self.endResetModel()
 
     def data(self, index: QModelIndex, role: int = Qt.DisplayRole) -> Any:
         if 0 <= index.row() < self.rowCount():
