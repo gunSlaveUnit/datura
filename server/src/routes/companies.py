09ad10e1c813b19c2d62136b7a0da15b9eeef65a
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2023-04-18 15:59:10 +0300

    Company create route

--- server/src/routes/companies.py
+++ server/src/routes/companies.py
@@ -6,8 +6,10 @@ from starlette import status
 from starlette.responses import Response
 
 from server.src.models.company import Company
+from server.src.models.user import User
 from server.src.schemas.company import CompanyCreateSchema, CompanyDBSchema, ApprovingSchema
 from server.src.settings import COMPANIES_ROUTER_PREFIX, Tags
+from server.src.utils.auth import get_current_user
 from server.src.utils.db import get_db
 
 router = APIRouter(prefix=COMPANIES_ROUTER_PREFIX, tags=[Tags.COMPANIES])
@@ -32,12 +34,30 @@ async def every(owner_id: int | None = None,
 
 
 @router.post('/', response_model=CompanyDBSchema)
-async def create(company: CompanyCreateSchema) -> CompanyDBSchema:
+async def create(company: CompanyCreateSchema,
+                 db: Session = Depends(get_db),
+                 current_user: User = Depends(get_current_user)) -> CompanyDBSchema:
     """
     Creating a new company.
     Return a CompanyDBScheme with created entity data.
     """
-    return CompanyDBSchema(title="Test company juridical name")
+
+    potentially_existing_company = db.query(Company).filter(Company.owner_id == current_user.id).first()
+
+    if potentially_existing_company:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="This user already has a registered company"
+        )
+
+    company = Company(**vars(company))
+    company.owner_id = current_user.id
+
+    db.add(company)
+    db.commit()
+    db.refresh(company)
+
+    return company
 
 
 @router.put('/{company_id}/', response_model=CompanyDBSchema)
