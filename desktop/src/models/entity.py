f2df63572526300faa08b1a2dfc3d93f147db0b8
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2023-04-22 12:03:53 +0300

    Entity model is inherited from QObject. It not so compact now

--- desktop/src/models/entity.py
+++ desktop/src/models/entity.py
@@ -1,9 +1,31 @@
 import datetime
-from dataclasses import dataclass
+
+from PySide6.QtCore import QObject, Signal, Property
 
 
-@dataclass(slots=True)
-class Entity:
-    id: int
-    created_at: datetime.datetime
-    last_updated_at: datetime.datetime | None
+class Entity(QObject):
+    def __init__(self,
+                 id: int = -1,
+                 created_at: datetime.datetime = 0,
+                 last_updated_at: datetime.datetime | None = None
+                 ):
+        super().__init__()
+
+        self._id = id
+        self._created_at = created_at
+        self._last_updated_at = last_updated_at
+
+    @Signal
+    def id_changed(self):
+        pass
+
+    @Property(int, notify=id_changed)
+    def id(self):
+        return self._id
+
+    @id.setter
+    def id(self, new_value: int):
+        if self._id == new_value:
+            return
+        self._id = new_value
+        self.id_changed.emit()
