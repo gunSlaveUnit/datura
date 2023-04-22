e5eb80f1de48df2d50cb9ad923612be5db489974
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2023-04-22 12:04:26 +0300

    Current user property in AuthService

--- desktop/src/services/AuthService.py
+++ desktop/src/services/AuthService.py
@@ -1,15 +1,31 @@
 import requests
+from PySide6.QtCore import QObject, Signal, Property
 
 from desktop.src.models.user import User
 from desktop.src.settings import REGISTER_URL, LOGIN_URL, ME_URL, LOGOUT_URL
 
 
-class AuthService:
+class AuthService(QObject):
     def __init__(self):
         super().__init__()
 
         self.session_id = None
-        self.current_user = None
+        self._current_user = None
+
+    @Signal
+    def current_user_changed(self):
+        pass
+
+    @Property(QObject, notify=current_user_changed)
+    def current_user(self):
+        return self._current_user
+
+    @current_user.setter
+    def current_user(self, new_value: QObject):
+        if self._current_user == new_value:
+            return
+        self._current_user = new_value
+        self.current_user_changed.emit()
 
     def sign_up(self, data):
         reply = requests.post(REGISTER_URL, json=data)
