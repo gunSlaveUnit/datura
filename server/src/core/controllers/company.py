2448c765ecc58148a61756237b693da16b364ac1
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2023-04-23 22:30:43 +0300

    Company controller to get a list of items and create a new one

---
+++ server/src/core/controllers/company.py
@@ -0,0 +1,34 @@
+from fastapi import Depends, HTTPException
+from starlette import status
+
+from server.src.core.logic.company import CompanyLogic
+from server.src.core.logic.user import UserLogic
+from server.src.core.models.company import Company
+from server.src.core.models.user import User
+from server.src.core.utils.db import get_db
+from server.src.schemas.company import CompanyCreateSchema
+
+
+class CompanyController:
+    def __init__(self, db=Depends(get_db)):
+        self.db = db
+        self.user_logic = UserLogic(db)
+        self.company_logic = CompanyLogic(db)
+
+    async def items(self):
+        items = await self.company_logic.items()
+        return items.all()
+
+    async def create(self, company_data: CompanyCreateSchema, current_user: User):
+        potentially_existing_company = await self.company_logic.item_by_owner(current_user.id)
+
+        if potentially_existing_company:
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                detail="This user already has a registered company"
+            )
+
+        company = Company(**vars(company_data))
+        company.owner_id = current_user.id
+
+        return await self.company_logic.create(company)
