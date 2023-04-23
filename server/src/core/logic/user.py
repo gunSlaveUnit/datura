d930316f3f2e043178fbde7fddb0fed0409f9c0a
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2023-04-23 17:12:47 +0300

    User create method

--- server/src/core/logic/user.py
+++ server/src/core/logic/user.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 from sqlalchemy.orm import Session, Query
 
 from server.src.core.models.user import User
@@ -9,3 +11,9 @@ class UserLogic:
 
     async def items(self) -> Query:
         return self.db.query(User)
+
+    async def create(self, item: User) -> User:
+        self.db.add(item)
+        self.db.commit()
+        self.db.refresh(item)
+        return item
