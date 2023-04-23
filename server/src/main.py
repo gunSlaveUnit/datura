55f707cbb75f1ca02d787644598d40ed72225372
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2023-04-23 15:03:04 +0300

    FastAPI debug mode sets from settings

--- server/src/main.py
+++ server/src/main.py
@@ -1,24 +1,11 @@
 from fastapi import FastAPI
 
-from server.src.routes.auth import router as auth_router
-from server.src.routes.games import router as games_router
-from server.src.routes.companies import router as companies_router
-from server.src.routes.library import router as library_router
-from server.src.routes.users import router as users_router
-from server.src.routes.cart import router as cart_router
-from server.src.settings import tags_metadata
+from server.src.settings import DEBUG
 from server.src.utils.db import Base, engine
 from server.src.utils.db_init import init_db
 
 
-app = FastAPI(openapi_tags=tags_metadata)
-
-app.include_router(auth_router)
-app.include_router(games_router)
-app.include_router(companies_router)
-app.include_router(library_router)
-app.include_router(users_router)
-app.include_router(cart_router)
+app = FastAPI(debug=DEBUG)
 
 
 @app.on_event("startup")
