6232126c68528bc5912b54c5283b21755f4654b2
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2023-04-23 22:57:02 +0300

    Method to change company approving

--- server/src/core/controllers/company.py
+++ server/src/core/controllers/company.py
@@ -2,11 +2,14 @@ from fastapi import Depends, HTTPException
 from starlette import status
 
 from server.src.core.logic.company import CompanyLogic
+from server.src.core.logic.game import GameLogic
 from server.src.core.logic.user import UserLogic
 from server.src.core.models.company import Company
+from server.src.core.models.game import Game
 from server.src.core.models.user import User
+from server.src.core.settings import GameStatusType
 from server.src.core.utils.db import get_db
-from server.src.schemas.company import CompanyCreateSchema
+from server.src.schemas.company import CompanyCreateSchema, ApprovingSchema
 
 
 class CompanyController:
@@ -14,6 +17,7 @@ class CompanyController:
         self.db = db
         self.user_logic = UserLogic(db)
         self.company_logic = CompanyLogic(db)
+        self.game_logic = GameLogic(db)
 
     async def items(self):
         items = await self.company_logic.items()
@@ -32,3 +36,11 @@ class CompanyController:
         company.owner_id = current_user.id
 
         return await self.company_logic.create(company)
+
+    def manage_approving(self, company_id: int, approving: ApprovingSchema):
+        company_owner = await self.user_logic.item_by_company(company_id)
+
+        if not approving.is_approved:
+            self.game_logic.items().filter(Game.owner_id == company_owner).update({"status_id": GameStatusType.NOT_SEND})
+
+        self.company_logic.update(company_id, {"is_approved": approving.is_approved})
