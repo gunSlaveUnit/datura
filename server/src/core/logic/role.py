d098095272f66c696d6a4290986577da87b00628
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2023-04-23 19:07:10 +0300

    User and role logic inherits Logic

--- server/src/core/logic/role.py
+++ server/src/core/logic/role.py
@@ -1,17 +1,15 @@
-from typing import Type, Optional
+from typing import Type, Any
 
 from sqlalchemy.orm import Session, Query
 
+from server.src.core.logic.logic import Logic
 from server.src.core.models.role import Role
 from server.src.core.settings import RoleType
 
 
-class RoleLogic:
-    def __init__(self, db: Session):
-        self.db = db
-
-    async def items(self) -> Query:
-        return self.db.query(Role)
+class RoleLogic(Logic):
+    def __init__(self, db: Session, *args: Any, **kwargs: Any):
+        super().__init__(Role, db, *args, **kwargs)
 
     async def item_by_title(self, title: RoleType) -> Type[Role]:
         return self.db.query(Role).filter(Role.title == title).one()
--- server/src/core/logic/user.py
+++ server/src/core/logic/user.py
@@ -1,40 +1,17 @@
-from typing import Optional
+from typing import Optional, Any
 
-from sqlalchemy.exc import NoResultFound
-from sqlalchemy.orm import Session, Query
+from sqlalchemy.orm import Session
 
+from server.src.core.logic.logic import Logic
 from server.src.core.models.user import User
 
 
-class UserLogic:
-    def __init__(self, db: Session):
-        self.db = db
-
-    async def items(self) -> Query:
-        return self.db.query(User)
+class UserLogic(Logic):
+    def __init__(self, db: Session, *args: Any, **kwargs: Any):
+        super().__init__(User, db, *args, **kwargs)
 
     async def item_by_email(self, email: str) -> Optional[User]:
         return self.db.query(User).filter(User.email == email).first()
 
     async def item_by_account_name(self, email: str) -> Optional[User]:
         return self.db.query(User).filter(User.account_name == email).first()
-
-    async def create(self, item: User) -> User:
-        self.db.add(item)
-        self.db.commit()
-        self.db.refresh(item)
-        return item
-
-    async def update(self, item_id: int, new_data: dict) -> User:
-        update_user_query = self.db.query(User).filter(User.id == item_id)
-
-        try:
-            updatable_user = update_user_query.one()
-        except NoResultFound:
-            raise ValueError(f"Item with {item_id} id not found")
-
-        update_user_query.update(new_data, synchronize_session=False)
-        self.db.commit()
-        self.db.refresh(updatable_user)
-
-        return updatable_user
