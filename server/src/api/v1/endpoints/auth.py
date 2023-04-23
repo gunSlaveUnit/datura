3be74024a6ebe2cf0887ea2dbe13e2daffe40bec
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2023-04-23 16:58:42 +0300

    Await all coroutines

--- server/src/api/v1/endpoints/auth.py
+++ server/src/api/v1/endpoints/auth.py
@@ -10,16 +10,16 @@ router = APIRouter(prefix=AUTH_ROUTER_PREFIX, tags=[Tags.AUTH])
 @router.post('/sign-up/')
 async def sign_up(registration_data: SignUpSchema,
                   user_controller: UserController = Depends(UserController)):
-    return user_controller.sign_up(registration_data)
+    return await user_controller.sign_up(registration_data)
 
 
 @router.post('/sign-in/')
 async def sign_in(login_data: SignInSchema,
                   user_controller: UserController = Depends(UserController)):
-    return user_controller.sign_in(login_data)
+    return await user_controller.sign_in(login_data)
 
 
 @router.post('/sign-out/')
 async def sign_out(session: str = Cookie(),
                    user_controller: UserController = Depends(UserController)):
-    return user_controller.sign_out(session)
+    return await user_controller.sign_out(session)
