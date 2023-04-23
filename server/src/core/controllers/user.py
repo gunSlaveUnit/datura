71c9959e4cc6d2778293620fe69e09ae8fea9ddf
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2023-04-23 16:57:25 +0300

    User logic object and test "items" method in controller

--- server/src/core/controllers/user.py
+++ server/src/core/controllers/user.py
@@ -1,7 +1,18 @@
+from fastapi import Depends
+
+from server.src.core.logic.user import UserLogic
+from server.src.core.utils.db import get_db
 from server.src.schemas.auth import SignUpSchema, SignInSchema
 
 
 class UserController:
+    def __init__(self, db = Depends(get_db)):
+        self.user_logic = UserLogic(db)
+
+    async def items(self):
+        items = await self.user_logic.items()
+        return items.all()
+
     async def sign_up(self, user_data: SignUpSchema):
         pass
 
