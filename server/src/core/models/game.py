a9b69b3a7d3eb3e6fd1de11c39682dca92bd89c0
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2023-04-23 22:00:45 +0300

    Temp removing of developer and publisher from Game model. Game bind to account, not to a company

--- server/src/core/models/game.py
+++ server/src/core/models/game.py
@@ -8,13 +8,11 @@ class Game(Entity):
 
     title = Column(String, index=True, nullable=False)
     release_date = Column(Integer, index=True)
-    developer = Column(String, index=True, nullable=False)
-    publisher = Column(String, index=True, nullable=False)
     short_description = Column(Text, nullable=False)
     long_description = Column(Text, nullable=False)
     price = Column(Float, default=0.0, nullable=False)
     directory = Column(String, nullable=False)
 
-    company_id = Column(Integer, ForeignKey("companies.id", ondelete="CASCADE"), index=True, nullable=False)
+    owner_id = Column(Integer, ForeignKey("users.id", ondelete="CASCADE"), index=True, nullable=False)
     status_id = Column(Integer, ForeignKey("game_statuses.id", ondelete="RESTRICT"), index=True, nullable=False)
     age_category_id = Column(Integer, ForeignKey("age_categories.id", ondelete="RESTRICT"), index=True, nullable=False)
