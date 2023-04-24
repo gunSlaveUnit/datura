5d9bddac3124b5938612b1013e617f8e7d1ba156
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2023-04-24 22:24:37 +0300

    URLS to connect to the server

--- desktop/src/settings.py
+++ desktop/src/settings.py
@@ -7,3 +7,12 @@ RESOURCES_DIR = BASE_DIR / 'resources'
 ICONS_DIR = RESOURCES_DIR / 'icons'
 SOURCES_DIR = BASE_DIR / 'src'
 LAYOUTS_DIR = SOURCES_DIR / 'gui'
+
+API_VERSION = 1
+
+SERVER_URL = f"http://localhost:8000/v{API_VERSION}/"
+AUTH_URL = SERVER_URL + "auth/"
+REGISTER_URL = AUTH_URL + "sign-up/"
+LOGIN_URL = AUTH_URL + "sign-in/"
+LOGOUT_URL = AUTH_URL + "sign-out/"
+ME_URL = AUTH_URL + "me/"
