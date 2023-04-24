096095d73b815d2fcc9a1ac968a9ea66d91a5f5d
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2023-04-24 11:21:19 +0300

    Title field in Game Status not an Enum, but string now

--- server/src/core/models/game_status.py
+++ server/src/core/models/game_status.py
@@ -1,10 +1,9 @@
-from sqlalchemy import Column, Enum
+from sqlalchemy import Column, String
 
 from server.src.core.models.entity import Entity
-from server.src.core.settings import GameStatusType
 
 
 class GameStatus(Entity):
     __tablename__ = "game_statuses"
 
-    title = Column(Enum(GameStatusType), unique=True, index=True, nullable=False)
+    title = Column(String, unique=True, index=True, nullable=False)
