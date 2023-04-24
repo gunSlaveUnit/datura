194e5bf2de4a67aeaf5369d82f34b9bf503b5627
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2023-04-24 11:28:27 +0300

    In game creation check that user has a company, and it is approved

--- server/src/core/controllers/game.py
+++ server/src/core/controllers/game.py
@@ -1,7 +1,9 @@
 import uuid
 
-from fastapi import Depends
+from fastapi import Depends, HTTPException
+from starlette import status
 
+from server.src.core.logic.company import CompanyLogic
 from server.src.core.logic.game import GameLogic
 from server.src.core.logic.game_status import GameStatusLogic
 from server.src.core.models.game import Game
@@ -15,6 +17,7 @@ class GameController:
     def __init__(self, db=Depends(get_db)):
         self.db = db
         self.game_logic = GameLogic(db)
+        self.company_logic = CompanyLogic(db)
         self.game_status_logic = GameStatusLogic(db)
 
     async def items(self):
@@ -22,6 +25,20 @@ class GameController:
         return items.all()
 
     async def create(self, game_data: GameCreateSchema, current_user: User):
+        potentially_not_existing_company = await self.company_logic.item_by_owner(current_user.id)
+
+        if potentially_not_existing_company is None:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail="Current user does not have a registered company"
+            )
+
+        if not potentially_not_existing_company.is_approved:
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                detail="Information about the user's company may be inaccurate. Creating is temporarily disabled"
+            )
+
         game = Game(**vars(game_data))
         game.owner_id = current_user.id
 
