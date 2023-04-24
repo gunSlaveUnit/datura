21b8bfe37c9e7450098dfb844df6ad02abb4c615
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2023-04-24 11:06:44 +0300

    Game "create" route

--- server/src/api/v1/endpoints/games.py
+++ server/src/api/v1/endpoints/games.py
@@ -1,7 +1,10 @@
-from fastapi import APIRouter, Body
+from fastapi import APIRouter, Body, Depends
 
+from server.src.core.controllers.game import GameController
+from server.src.core.models.user import User
 from server.src.core.settings import Tags, GAMES_ROUTER_PREFIX, GameStatusType
-from server.src.schemas.game import GameFilterSchema
+from server.src.core.utils.auth import get_current_user
+from server.src.schemas.game import GameFilterSchema, GameCreateSchema
 
 router = APIRouter(prefix=GAMES_ROUTER_PREFIX, tags=[Tags.GAMES])
 
@@ -13,3 +16,10 @@ async def items(game_filter: GameFilterSchema = Body(None)):
         game_filter = GameFilterSchema(status_id=[GameStatusType.PUBLISHED])
 
     return game_filter
+
+
+@router.post('/')
+async def create(new_game_data: GameCreateSchema,
+                 current_user: User = Depends(get_current_user),
+                 game_controller: GameController = Depends(GameController)):
+    return await game_controller.create(new_game_data, current_user)
