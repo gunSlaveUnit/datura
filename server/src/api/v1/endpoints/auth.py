4c2d49a01a8ca60b49c358213e61d7289ff3dc34
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2023-04-24 21:12:00 +0300

    Me route

--- server/src/api/v1/endpoints/auth.py
+++ server/src/api/v1/endpoints/auth.py
@@ -1,6 +1,8 @@
 from fastapi import APIRouter, Depends, Cookie
 
 from server.src.core.controllers.user import UserController
+from server.src.core.models.user import User
+from server.src.core.utils.auth import get_current_user
 from server.src.schemas.auth import SignUpSchema, SignInSchema
 from server.src.core.settings import Tags, AUTH_ROUTER_PREFIX
 
@@ -23,3 +25,8 @@ async def sign_in(login_data: SignInSchema,
 async def sign_out(session: str = Cookie(),
                    user_controller: UserController = Depends(UserController)):
     return await user_controller.sign_out(session)
+
+
+@router.get('/me/')
+async def me(current_user: User = Depends(get_current_user)):
+    return current_user
