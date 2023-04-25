8cb409344391c3b0b0ac525e39467db34d66bde6
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2023-04-25 14:40:09 +0300

    Cart router

---
+++ server/src/api/v1/endpoints/cart.py
@@ -0,0 +1,49 @@
+from typing import List, Type
+
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
+from starlette import status
+from starlette.responses import Response
+
+from server.src.api.v1.schemas.cart import CartJoinedSchema, CartDBSchema, CartCreateSchema
+from server.src.core.models.cart import Cart
+from server.src.core.models.library import Library
+from server.src.core.models.user import User
+from server.src.core.settings import CART_ROUTER_PREFIX, Tags
+from server.src.core.utils.auth import get_current_user
+from server.src.core.utils.db import get_db
+
+router = APIRouter(prefix=CART_ROUTER_PREFIX, tags=[Tags.CART])
+
+
+@router.get('/', response_model=List[CartJoinedSchema | CartDBSchema])
+async def items(db: Session = Depends(get_db),
+                current_user: User = Depends(get_current_user)) -> list[Type[Cart]]:
+    return db.query(Cart).filter(Cart.buyer_id == current_user.id).all()
+
+
+@router.post('/', response_model=CartDBSchema)
+async def create(new_record: CartCreateSchema,
+                 db: Session = Depends(get_db),
+                 current_user: User = Depends(get_current_user)) -> Type[Cart]:
+    record = Cart(**vars(new_record))
+    record.buyer_id = current_user.id
+
+    return await Cart.create(db, record)
+
+
+@router.post('/pay/')
+async def pay(db: Session = Depends(get_db),
+              current_user: User = Depends(get_current_user)):
+    user_cart = await items(db, current_user)
+
+    for record in user_cart:
+        new_library_record = Library(
+            player_id=current_user.id,
+            game_id=record.game_id
+        )
+
+        await Cart.create(db, new_library_record)
+        await Cart.delete(db, record.id)
+
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
