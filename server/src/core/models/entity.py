2d28fba643ac0fb35bbe6b9cb371103a4fb9a3e1
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2023-04-25 11:43:02 +0300

    Method to get an entity by id

--- server/src/core/models/entity.py
+++ server/src/core/models/entity.py
@@ -1,6 +1,10 @@
 import datetime
 
+from fastapi import HTTPException
 from sqlalchemy import Column, Integer
+from sqlalchemy.exc import NoResultFound
+from sqlalchemy.orm import Session
+from starlette import status
 
 from server.src.core.utils.db import Base
 
@@ -11,6 +15,19 @@ class Entity(Base):
     id = Column(Integer, primary_key=True, index=True, autoincrement=True)
     created_at = Column(Integer, default=datetime.datetime.now().timestamp(), nullable=False)
     updated_at = Column(Integer, onupdate=datetime.datetime.now().timestamp())
+
+    @classmethod
+    def by_id(cls, db: Session, item_id: int):
+        item_query = db.query(cls).filter(cls.id == item_id)
+        try:
+            item = item_query.one()
+        except NoResultFound:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail="Item with this id not found"
+            )
+
+        return item
 
     def dict(self) -> dict:
         """
