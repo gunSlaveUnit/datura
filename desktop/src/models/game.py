e9e447a7bb063f1ec273589d22593095d135b489
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2023-04-25 15:33:24 +0300

    Game models

---
+++ desktop/src/models/game.py
@@ -0,0 +1,62 @@
+from dataclasses import fields, dataclass
+from typing import Any
+
+import requests
+from PySide6.QtCore import QAbstractListModel, QModelIndex, QByteArray, Qt, Slot
+
+from desktop.src.models.entity import Entity
+from desktop.src.services.AuthService import AuthService
+from desktop.src.settings import GAMES_URL
+
+
+@dataclass()
+class Game(Entity):
+    title: str
+    release_date: int | None
+    developer: str
+    publisher: str
+    short_description: str
+    long_description: str
+    price: float
+    status_id: int
+    age_category_id: int
+    directory: str
+    company_id: int
+
+
+class GameList(QAbstractListModel):
+    def __init__(self, auth_service: AuthService):
+        super().__init__()
+
+        self._auth_service = auth_service
+
+        self._games = []
+
+    @Slot()
+    def load_store(self):
+        reply = self._auth_service.authorized_session.get(GAMES_URL)
+
+        if reply.status_code == requests.codes.ok:
+            games = reply.json()
+
+            self.beginResetModel()
+            self._games = []
+            for game in games:
+                self._games.append(Game(**game))
+            self.endResetModel()
+
+    def data(self, index: QModelIndex, role: int = Qt.DisplayRole) -> Any:
+        if 0 <= index.row() < self.rowCount():
+            game = self._games[index.row()]
+            name = self.roleNames().get(role)
+            if name:
+                return getattr(game, name.decode())
+
+    def roleNames(self) -> dict[int, QByteArray]:
+        d = {}
+        for i, field in enumerate(fields(Game)):
+            d[Qt.DisplayRole + i] = field.name.encode()
+        return d
+
+    def rowCount(self, index: QModelIndex = QModelIndex()) -> int:
+        return len(self._games)
