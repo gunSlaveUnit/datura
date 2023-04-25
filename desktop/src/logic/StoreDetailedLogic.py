ed643e1c73ba16cd560448ca1ee06ed16642c86b
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2023-04-25 22:04:29 +0300

    Store detailed logic with game status (where is it: in cart, in store or in library) checking

---
+++ desktop/src/logic/StoreDetailedLogic.py
@@ -0,0 +1,85 @@
+from enum import Enum
+
+import requests
+from PySide6.QtCore import QObject, Signal, Slot, Property
+
+from desktop.src.services.AuthService import AuthService
+from desktop.src.settings import GAMES_URL, LIBRARY_URL, CART_URL
+
+
+class GameLocation(int, Enum):
+    IN_STORE = 0
+    IN_LIBRARY = 1
+    IN_CART = 2
+
+
+class StoreDetailedLogic(QObject):
+    game_title_changed = Signal()
+    game_location_changed = Signal()
+
+    def __init__(self, auth_service: AuthService):
+        super().__init__()
+
+        self._auth_service = auth_service
+
+        self._game_id = -1
+        self._game_title = ''
+        self._game_location: int = GameLocation.IN_STORE
+
+    # region Game title
+
+    @Property(str, notify=game_title_changed)
+    def game_title(self):
+        return self._game_title
+
+    @game_title.setter
+    def game_title(self, new_value: str):
+        if self._game_title != new_value:
+            self._game_title = new_value
+            self.game_title_changed.emit()
+
+    # endregion
+
+    # region Game location
+
+    @Property(int, notify=game_location_changed)
+    def game_location(self):
+        return self._game_location
+
+    @game_location.setter
+    def game_location(self, new_value: int):
+        if self._game_location != new_value:
+            self._game_location = new_value
+            self.game_location_changed.emit()
+
+    # endregion
+
+    @Slot(int)
+    def load(self, game_id: int):
+        reply = self._auth_service.authorized_session.get(GAMES_URL + f'{game_id}/')
+        if reply.status_code == requests.codes.ok:
+            data = reply.json()
+
+            self._game_id = data["id"]
+            self.game_title = data["title"]
+
+            self._set_game_location_status()
+
+    def _set_game_location_status(self):
+        if self._check_game_in_library():
+            self.game_location = GameLocation.IN_LIBRARY
+        elif self._check_game_in_cart():
+            self.game_location = GameLocation.IN_CART
+        else:
+            self.game_location = GameLocation.IN_STORE
+
+    def _check_game_in_library(self):
+        user_id = self._auth_service.current_user.id
+        reply = self._auth_service.authorized_session.get(
+            LIBRARY_URL + f"?user_id={user_id}&game_id={self._game_id}"
+        )
+        return reply.ok and reply.json()
+
+    def _check_game_in_cart(self):
+        reply = self._auth_service.authorized_session.get(CART_URL + f'?game_id={self._game_id}')
+        return reply.ok and reply.json()
--- desktop/src/main.py
+++ desktop/src/main.py
@@ -5,6 +5,7 @@ from PySide6.QtGui import QGuiApplication, QColor, QIcon
 from PySide6.QtQml import QQmlApplicationEngine
 
 from desktop.src.logic.AuthLogic import AuthLogic
+from desktop.src.logic.StoreDetailedLogic import StoreDetailedLogic
 from desktop.src.models.game import GameList
 from desktop.src.services.AuthService import AuthService
 from desktop.src.settings import ICONS_DIR, LAYOUTS_DIR, APP_ID
@@ -25,6 +26,9 @@ if __name__ == '__main__':
 
     auth_logic = AuthLogic(auth_service)
     engine.rootContext().setContextProperty("auth_logic", auth_logic)
+
+    store_detailed_logic = StoreDetailedLogic(auth_service)
+    engine.rootContext().setContextProperty("store_detailed_logic", store_detailed_logic)
 
     game_list_model = GameList(auth_service)
     engine.rootContext().setContextProperty("game_list_model", game_list_model)
