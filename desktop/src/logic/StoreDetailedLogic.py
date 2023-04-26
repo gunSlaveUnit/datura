fc9bf15d835d0f2205ef0b83cfccfa0417ef603c
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2023-04-26 10:59:19 +0300

    Price property

--- desktop/src/logic/StoreDetailedLogic.py
+++ desktop/src/logic/StoreDetailedLogic.py
@@ -1,6 +1,5 @@
 from enum import Enum
 
-import requests
 from PySide6.QtCore import QObject, Signal, Slot, Property
 
 from desktop.src.services.AuthService import AuthService
@@ -14,43 +13,59 @@ class GameLocation(int, Enum):
 
 
 class StoreDetailedLogic(QObject):
-    game_title_changed = Signal()
-    game_location_changed = Signal()
+    title_changed = Signal()
+    price_changed = Signal()
+    location_changed = Signal()
 
     def __init__(self, auth_service: AuthService):
         super().__init__()
 
         self._auth_service = auth_service
 
-        self._game_id = -1
-        self._game_title = ''
-        self._game_location: int = GameLocation.IN_STORE
+        self._id = -1
+        self._title = ''
+        self._price = 0.0
+        self._location: int = GameLocation.IN_STORE
 
-    # region Game title
+    # region Title
 
-    @Property(str, notify=game_title_changed)
-    def game_title(self):
-        return self._game_title
+    @Property(str, notify=title_changed)
+    def title(self):
+        return self._title
 
-    @game_title.setter
-    def game_title(self, new_value: str):
-        if self._game_title != new_value:
-            self._game_title = new_value
-            self.game_title_changed.emit()
+    @title.setter
+    def title(self, new_value: str):
+        if self._title != new_value:
+            self._title = new_value
+            self.title_changed.emit()
 
     # endregion
 
-    # region Game location
+    # region Price
 
-    @Property(int, notify=game_location_changed)
-    def game_location(self):
-        return self._game_location
+    @Property(float, notify=price_changed)
+    def price(self):
+        return self._price
 
-    @game_location.setter
-    def game_location(self, new_value: int):
-        if self._game_location != new_value:
-            self._game_location = new_value
-            self.game_location_changed.emit()
+    @price.setter
+    def price(self, new_value: float):
+        if self._price != new_value:
+            self._price = new_value
+            self.price_changed.emit()
+
+    # endregion
+
+    # region Location
+
+    @Property(int, notify=location_changed)
+    def location(self):
+        return self._location
+
+    @location.setter
+    def location(self, new_value: int):
+        if self._location != new_value:
+            self._location = new_value
+            self.location_changed.emit()
 
     # endregion
 
@@ -60,8 +75,9 @@ class StoreDetailedLogic(QObject):
         if response.ok:
             data = response.json()
 
-            self._game_id = data["id"]
-            self.game_title = data["title"]
+            self._id = data["id"]
+            self.title = data["title"]
+            self.price = data["price"]
 
             self._set_game_location_status()
 
@@ -76,10 +92,10 @@ class StoreDetailedLogic(QObject):
     def _check_game_in_library(self):
         user_id = self._auth_service.current_user.id
         response = self._auth_service.authorized_session.get(
-            LIBRARY_URL + f"?user_id={user_id}&game_id={self._game_id}"
+            LIBRARY_URL + f"?user_id={user_id}&game_id={self._id}"
         )
         return response.ok and response.json()
 
     def _check_game_in_cart(self):
-        response = self._auth_service.authorized_session.get(CART_URL + f'?game_id={self._game_id}')
+        response = self._auth_service.authorized_session.get(CART_URL + f'?game_id={self._id}')
         return response.ok and response.json()
