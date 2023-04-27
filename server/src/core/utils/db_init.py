1e81f9a95ac84d659ffd76d973b96ed3c7d85d62
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2023-04-27 18:15:57 +0300

    Changed descriptions of age categories

--- server/src/core/utils/db_init.py
+++ server/src/core/utils/db_init.py
@@ -60,22 +60,22 @@ def _add_game_statuses(session):
 
 def _add_ages(session):
     pegi_3 = AgeCategory(title=AgeType.PEGI_3,
-                         description="Suitable for all age categories. The game must not contain sounds "
+                         description="The game must not contain sounds "
                                      "or images that may frighten young children. Profanity must not be"
                                      " used.")
-    pegi_7 = AgeCategory(title=AgeType.PEGI_7, description="Suitable for people over 7 years old. The game may contain "
+    pegi_7 = AgeCategory(title=AgeType.PEGI_7, description="The game may contain "
                                                            "moderate violence and some scenes may scare children.")
     pegi_12 = AgeCategory(title=AgeType.PEGI_12,
-                          description="Suitable for people over 12 years old. The game may contain "
+                          description="The game may contain "
                                       "more realistic and naturalistic scenes of violence.")
     pegi_16 = AgeCategory(title=AgeType.PEGI_16,
-                          description="Suitable for people over 16 years old. This rating is used when "
+                          description="This rating is used when "
                                       "the violence becomes realistic and lifelike. Profanity, "
                                       "use of tobacco, alcohol, or illegal drugs may also be present.")
-    pegi_18 = AgeCategory(title=AgeType.PEGI_18, description='Suitable for persons over 18 years of age. The "adult" '
-                                                             'classification is used when an extreme level of violence and '
-                                                             'unmotivated murder is present in the game. Romanticization of '
-                                                             'drugs, gambling, and sexual activity may also be present.')
+    pegi_18 = AgeCategory(title=AgeType.PEGI_18, description='The "adult" classification is used when an extreme '
+                                                             'level of violence and unmotivated murder is present in '
+                                                             'the game. Romanticization of drugs, gambling, and sexual '
+                                                             'activity may also be present.')
 
     session.add(pegi_3)
     session.add(pegi_7)
