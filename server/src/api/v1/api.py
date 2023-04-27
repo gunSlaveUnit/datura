522efe57cd3a9780607cc223ee56351299299240
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2023-04-27 18:17:19 +0300

    Added route to get age categories

--- server/src/api/v1/api.py
+++ server/src/api/v1/api.py
@@ -4,7 +4,8 @@ from server.src.core.settings import Tags, API_VERSION_1_PREFIX
 from server.src.api.v1.endpoints.auth import router as auth_router
 from server.src.api.v1.endpoints.games import router as games_router
 from server.src.api.v1.endpoints.companies import router as companies_router
-from server.src.api.v1.endpoints.users import router as users_router
+from server.src.api.v1.endpoints.users import router as age_categories_router
+from server.src.api.v1.endpoints.age_caterogies import router as users_router
 from server.src.api.v1.endpoints.library import router as library_router
 from server.src.api.v1.endpoints.cart import router as cart_router
 
@@ -14,5 +15,6 @@ router.include_router(auth_router)
 router.include_router(games_router)
 router.include_router(companies_router)
 router.include_router(users_router)
+router.include_router(age_categories_router)
 router.include_router(library_router)
 router.include_router(cart_router)
