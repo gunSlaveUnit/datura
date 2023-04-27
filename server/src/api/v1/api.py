5bd2bd5255007be18a3a8b266c28024cbbff6ef9
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2023-04-27 23:00:11 +0300

    Platforms router

--- server/src/api/v1/api.py
+++ server/src/api/v1/api.py
@@ -5,6 +5,7 @@ from server.src.api.v1.endpoints.auth import router as auth_router
 from server.src.api.v1.endpoints.games import router as games_router
 from server.src.api.v1.endpoints.companies import router as companies_router
 from server.src.api.v1.endpoints.users import router as age_categories_router
+from server.src.api.v1.endpoints.platforms import  router as platforms_router
 from server.src.api.v1.endpoints.age_caterogies import router as users_router
 from server.src.api.v1.endpoints.library import router as library_router
 from server.src.api.v1.endpoints.cart import router as cart_router
@@ -16,5 +17,6 @@ router.include_router(games_router)
 router.include_router(companies_router)
 router.include_router(users_router)
 router.include_router(age_categories_router)
+router.include_router(platforms_router)
 router.include_router(library_router)
 router.include_router(cart_router)
