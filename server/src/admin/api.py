5f034bc39ed3b7a43f338db952b8a21ffcd0fd1a
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2023-04-29 22:27:04 +0300

    Protected home route

--- server/src/admin/api.py
+++ server/src/admin/api.py
@@ -1,14 +1,17 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
 from starlette.requests import Request
 from starlette.responses import HTMLResponse
 
-from server.src.core.settings import ADMIN_ROUTER_PREFIX, Tags, templates
+from server.src.core.models.user import User
+from server.src.core.settings import ADMIN_ROUTER_PREFIX, Tags, templates, RoleType
 from server.src.admin.endpoints.login import router as login_router
+from server.src.core.utils.auth import GetCurrentUser
 
 router = APIRouter(prefix=ADMIN_ROUTER_PREFIX, tags=[Tags.ADMIN])
 router.include_router(login_router)
 
 
 @router.get('/', response_class=HTMLResponse)
-async def home(request: Request):
+async def home(request: Request,
+               current_user: User = Depends(GetCurrentUser(scopes=(RoleType.ADMIN,)))):
     return templates.TemplateResponse("home.html", {"request": request})
