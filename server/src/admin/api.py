85947e48eefdb7d091985ca0a753519c4990ef1d
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2023-04-29 22:06:10 +0300

    Registered login router

--- server/src/admin/api.py
+++ server/src/admin/api.py
@@ -1,7 +1,14 @@
 from fastapi import APIRouter
+from starlette.requests import Request
+from starlette.responses import HTMLResponse
 
-from server.src.core.settings import ADMIN_ROUTER_PREFIX, Tags
-from server.src.admin.endpoints.home import router as home_router
+from server.src.core.settings import ADMIN_ROUTER_PREFIX, Tags, templates
+from server.src.admin.endpoints.login import router as login_router
 
 router = APIRouter(prefix=ADMIN_ROUTER_PREFIX, tags=[Tags.ADMIN])
-router.include_router(home_router)
+router.include_router(login_router)
+
+
+@router.get('/', response_class=HTMLResponse)
+async def home(request: Request):
+    return templates.TemplateResponse("home.html", {"request": request})
