57c9a33c57d565877958d73e3cf3df54856eb508
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2023-04-30 19:51:20 +0300

    Concrete origin

--- server/src/main.py
+++ server/src/main.py
@@ -12,7 +12,7 @@ app = FastAPI(debug=DEBUG)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["http://127.0.0.1:8000"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
