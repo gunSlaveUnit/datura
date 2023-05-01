a77c4361c897ca3c97fcd7339c80d017d750f63e
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2023-05-01 16:30:09 +0300

    Relationship with platform

--- server/src/core/models/build.py
+++ server/src/core/models/build.py
@@ -1,4 +1,5 @@
 from sqlalchemy import Column, Integer, ForeignKey, String
+from sqlalchemy.orm import relationship
 
 from server.src.core.models.entity import Entity
 
@@ -11,4 +12,6 @@ class Build(Entity):
     params = Column(String)
 
     game_id = Column(Integer, ForeignKey("games.id", ondelete="CASCADE"), index=True, nullable=False)
+
     platform_id = Column(Integer, ForeignKey("platforms.id", ondelete="RESTRICT"), index=True, nullable=False)
+    platform = relationship("Platform", backref="platforms")
