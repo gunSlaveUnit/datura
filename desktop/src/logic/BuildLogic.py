565aeac3a3dbe55ba9aeb2e7d6454c41e22cfbfd
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2023-05-01 11:50:19 +0300

    Call property with dummy map method to get a 'call' field of a concrete build

--- desktop/src/logic/BuildLogic.py
+++ desktop/src/logic/BuildLogic.py
@@ -1,4 +1,6 @@
-from PySide6.QtCore import QObject, Slot
+from PySide6.QtCore import QObject, Slot, Signal, Property
+
+from desktop.src.settings import GAMES_URL
 
 
 class BuildLogic(QObject):
@@ -7,6 +9,26 @@ class BuildLogic(QObject):
 
         self._auth_service = auth_service
 
+        self._id = -1
+        self._call = ''
+
+    call_changed = Signal()
+
+    @Property(str, notify=call_changed)
+    def call(self):
+        return self._call
+
+    @call.setter
+    def call(self, new_value: str):
+        if self._call != new_value:
+            self._call = new_value
+            self.call_changed.emit()
+
     @Slot(int)
-    def map(self, game_id: int):
-        print(game_id)
+    def map(self, game_id, build_id: int):
+        response = self._auth_service.authorized_session.get(GAMES_URL + f'{game_id}/builds/{build_id}/')
+
+        if response.ok:
+            data = response.json()
+
+            self.call = data['call']
