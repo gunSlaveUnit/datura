adcb23b3649783d15ec0f44b81aebaf2e3a0413b
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2023-05-04 12:47:46 +0300

    Method to return a user balance

--- server/src/api/v1/endpoints/users.py
+++ server/src/api/v1/endpoints/users.py
@@ -1,15 +1,40 @@
+import decimal
+
 from fastapi import APIRouter, Depends, UploadFile, HTTPException
+from sqlalchemy import func
 from sqlalchemy.orm import Session
 from starlette import status
-from starlette.responses import FileResponse
+from starlette.responses import FileResponse, JSONResponse
 
+from server.src.core.models.payment import Payment
+from server.src.core.models.purchase import Purchase
 from server.src.core.models.user import User
 from server.src.core.settings import Tags, AVATARS_PATH, DEFAULT_AVATAR_FILENAME, USERS_ROUTER_PREFIX
-from server.src.core.utils.auth import _get_current_user, GetCurrentUser
+from server.src.core.utils.auth import GetCurrentUser
 from server.src.core.utils.db import get_db
 from server.src.core.utils.io import remove, save
 
 router = APIRouter(prefix=USERS_ROUTER_PREFIX, tags=[Tags.USERS])
+
+
+@router.get('/{user_id}/balance/')
+async def balance(user_id: int,
+                  db: Session = Depends(get_db),
+                  current_user: User = Depends(GetCurrentUser())):
+    if current_user != user_id or not current_user.is_superuser:
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="Permission denied: you cannot receive this information"
+        )
+
+    payments_total_cost = db.query(func.sum(Payment.amount)).filter(Payment.user_id == user_id).scalar()
+    purchases_total_cost = db.query(func.sum(Purchase.price)).filter(Purchase.buyer_id == user_id).scalar()
+
+    current_balance = decimal.Decimal(payments_total_cost) - decimal.Decimal(purchases_total_cost)
+
+    return JSONResponse({
+        "balance": current_balance
+    })
 
 
 @router.get('/{user_id}/avatar/')
