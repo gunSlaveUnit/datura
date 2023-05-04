74b6906ef9e26cdd4bdc1711d84d72cb7aedcffa
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2023-05-04 18:53:03 +0300

    No need current user in auth service

--- desktop/src/services/AuthService.py
+++ desktop/src/services/AuthService.py
@@ -1,30 +1,17 @@
 from typing import Union
 
 import requests
-from PySide6.QtCore import QObject, Signal, Property
+from PySide6.QtCore import QObject
 
 from common.api.v1.schemas.auth import SignUpSchema, SignInSchema
 from desktop.src.settings import REGISTER_URL, LOGIN_URL, LOGOUT_URL
 
 
 class AuthService(QObject):
-    current_user_changed = Signal()
-
     def __init__(self):
         super().__init__()
 
         self.authorized_session = None
-        self._current_user = None
-
-    @Property(QObject, notify=current_user_changed)
-    def current_user(self):
-        return self._current_user
-
-    @current_user.setter
-    def current_user(self, new_value: QObject):
-        if self._current_user != new_value:
-            self._current_user = new_value
-            self.current_user_changed.emit()
 
     def _user_access(self, url: str, data: Union[SignUpSchema, SignInSchema]):
         response = requests.post(url, json=data.dict())
@@ -46,6 +33,5 @@ class AuthService(QObject):
 
         if response.ok:
             self.authorized_session = None
-            self.current_user = None
 
         return response
