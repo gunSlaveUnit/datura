0375e6bd30da13a213b7db8dc059276b875ad2f1
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2023-05-05 14:27:14 +0300

    Cart service

---
+++ desktop/src/services/CartService.py
@@ -0,0 +1,18 @@
+from typing import Optional
+
+from PySide6.QtCore import QObject
+
+from desktop.src.services.AuthService import AuthService
+from desktop.src.settings import LIBRARY_URL, CART_URL
+
+
+class CartService(QObject):
+    def __init__(self, auth_service: AuthService):
+        super().__init__()
+
+        self._auth_service: AuthService = auth_service
+
+    def check(self, item_id: int):
+        params = {"game_id": item_id}
+        response = self._auth_service.authorized_session.get(CART_URL, params=params)
+        return response.ok and response.json()
