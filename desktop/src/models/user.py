2c44619e5bca286777c219196af0e007462f9147
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2023-05-05 13:59:54 +0300

    Removed useless methods from user desktop model

--- desktop/src/models/user.py
+++ desktop/src/models/user.py
@@ -1,14 +1,10 @@
 from dataclasses import dataclass
 
-from PySide6.QtCore import Property, Signal, QObject
-
 from desktop.src.models.entity import Entity
 
 
 @dataclass
 class User(Entity):
-    name_changed = Signal()
-
     email: str
     account_name: str
     displayed_name: str
