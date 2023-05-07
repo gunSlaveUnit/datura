2323cca307352af415f32dab9c8c799aefa5529b
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2023-05-07 20:26:28 +0300

    Library update schema

--- common/api/v1/schemas/library.py
+++ common/api/v1/schemas/library.py
@@ -1,5 +1,15 @@
+from pydantic import BaseModel
+
 from common.api.v1.schemas.entity import EntityDBSchema
 from common.api.v1.schemas.game import GameDBSchema
+
+
+class LibraryUpdateSchema(BaseModel):
+    game_time: int
+    last_run: int | None
+
+    class Config:
+        orm_mode = True
 
 
 class LibraryDBSchema(EntityDBSchema):
